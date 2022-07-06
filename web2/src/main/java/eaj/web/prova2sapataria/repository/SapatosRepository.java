e12e9cc93a4603f0dfb8ca8b710c151067137d54
Paulo Arthur <93049254+Reynald-V1@users.noreply.github.com>
2022-07-06 10:03:51 -0300

    Delete web2 directory

--- web2/src/main/java/eaj/web/prova2sapataria/repository/SapatosRepository.java
+++
@@ -1,8 +0,0 @@
-package eaj.web.prova2sapataria.repository;
-
-import eaj.web.prova2sapataria.model.Sapatos;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SapatosRepository extends JpaRepository<Sapatos, Long> {
-    Sapatos findSapatosById(Long Id);
-}
--- web2/src/main/java/eaj/web/prova2sapataria/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package eaj.web.prova2sapataria.repository;
-
-import eaj.web.prova2sapataria.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserById(Long Id);
-}
